e8c3269ed11efb1ec18b03d1c567088beec36486
Themainbased <ardikece17@gmail.com>
2023-02-16 11:54:00 +0700

    Dashboard Demo UI Dev

--- app/Http/Controllers/ArtikelController.php
+++ app/Http/Controllers/ArtikelController.php
@@ -3,31 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ArtikelController extends Controller
 {
     public function index()
     {
     	// mengambil data dari table pegawai
-    	$artikel = DB::table('data_spbe')->get();
+    	// $artikel = DB::table('data_spbe')->get();
  
     	// mengirim data pegawai ke view index
-    	return view('index',['pegawai' => $artikel]);
+    	return view('dashboard');
  
     }
 
     public function store(Request $request)
 {
 	// insert data ke table pegawai
-	DB::table('data_spbe')->insert([
-		'id' => $request->id,
-		'kd_artikel' => $request->kd_artikel,
-		'judul' => $request->judul,
-		'tgl_upload' => $request->tgl_upload,
-        'gambar' => $request->gambar
-	]);
-	// alihkan halaman ke halaman pegawai
-	return redirect('/');
+	// DB::table('data_spbe')->insert([
+	// 	'id' => $request->id,
+	// 	'kd_artikel' => $request->kd_artikel,
+	// 	'judul' => $request->judul,
+	// 	'tgl_upload' => $request->tgl_upload,
+    //     'gambar' => $request->gambar
+	// ]);
+	// // alihkan halaman ke halaman pegawai
+	// return redirect('/');
  
 }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="dashboard/styles.css" />
-    <title>Bootstap 5 Responsive Admin Dashboard</title>
+    <link rel="stylesheet" href="dashboard/style.css" />
+    <title>Dashboard | Spbe</title>
 </head>
 
 <body>
@@ -77,9 +77,9 @@
                         <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
                                 <h3 class="fs-2">720</h3>
-                                <p class="fs-5">Products</p>
+                                <p class="fs-5">Pengguna</p>
                             </div>
-                            <i class="fas fa-gift fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                            <i class="fas fa-user fs-1 primary-text border rounded-full secondary-bg p-3"></i>
                         </div>
                     </div>
 
@@ -87,10 +87,10 @@
                         <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
                                 <h3 class="fs-2">4920</h3>
-                                <p class="fs-5">Sales</p>
+                                <p class="fs-5">Article</p>
                             </div>
                             <i
-                                class="fas fa-hand-holding-usd fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                                class="fas fa-newspaper fs-1 primary-text border rounded-full secondary-bg p-3"></i>
                         </div>
                     </div>
 
@@ -98,17 +98,17 @@
                         <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
                                 <h3 class="fs-2">3899</h3>
-                                <p class="fs-5">Delivery</p>
+                                <p class="fs-5">Penulis</p>
                             </div>
-                            <i class="fas fa-truck fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                            <i class="fas fa-pen fs-1 primary-text border rounded-full secondary-bg p-3"></i>
                         </div>
                     </div>
 
                     <div class="col-md-3">
                         <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 class="fs-2">%25</h3>
-                                <p class="fs-5">Increase</p>
+                                <h3 class="fs-2">213</h3>
+                                <p class="fs-5">Pengunjung</p>
                             </div>
                             <i class="fas fa-chart-line fs-1 primary-text border rounded-full secondary-bg p-3"></i>
                         </div>
@@ -116,89 +116,23 @@
                 </div>
 
                 <div class="row my-5">
-                    <h3 class="fs-4 mb-3">Recent Orders</h3>
+                    <h3 class="fs-4 mb-3">Recent User</h3>
                     <div class="col">
                         <table class="table bg-white rounded shadow-sm  table-hover">
                             <thead>
                                 <tr>
                                     <th scope="col" width="50">#</th>
-                                    <th scope="col">Product</th>
-                                    <th scope="col">Customer</th>
-                                    <th scope="col">Price</th>
+                                    <th scope="col">Penulis</th>
+                                    <th scope="col">Judul</th>
+                                    <th scope="col">Dibuat</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <th scope="row">1</th>
-                                    <td>Television</td>
-                                    <td>Jonny</td>
-                                    <td>$1200</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Laptop</td>
-                                    <td>Kenny</td>
-                                    <td>$750</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Cell Phone</td>
-                                    <td>Jenny</td>
-                                    <td>$600</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">4</th>
-                                    <td>Fridge</td>
-                                    <td>Killy</td>
-                                    <td>$300</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">5</th>
-                                    <td>Books</td>
-                                    <td>Filly</td>
-                                    <td>$120</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">6</th>
-                                    <td>Gold</td>
-                                    <td>Bumbo</td>
-                                    <td>$1800</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">7</th>
-                                    <td>Pen</td>
-                                    <td>Bilbo</td>
-                                    <td>$75</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">8</th>
-                                    <td>Notebook</td>
-                                    <td>Frodo</td>
-                                    <td>$36</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">9</th>
-                                    <td>Dress</td>
-                                    <td>Kimo</td>
-                                    <td>$255</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">10</th>
-                                    <td>Paint</td>
-                                    <td>Zico</td>
-                                    <td>$434</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">11</th>
-                                    <td>Carpet</td>
-                                    <td>Jeco</td>
-                                    <td>$1236</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">12</th>
-                                    <td>Food</td>
-                                    <td>Haso</td>
-                                    <td>$422</td>
+                                    <td>Digital Interaction</td>
+                                    <td>Main PB Full Cash 100% No Root</td>
+                                    <td>19/02/2024 12:45</td>
                                 </tr>
                             </tbody>
                         </table>
