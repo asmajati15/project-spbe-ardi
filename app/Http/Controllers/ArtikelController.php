25d1499130f1f66ed451e57a9820d2ae60b10d8a
Themainbased <ardikece17@gmail.com>
2023-02-16 09:59:25 +0700

    Update Insert

--- app/Http/Controllers/ArtikelController.php
+++ app/Http/Controllers/ArtikelController.php
@@ -9,21 +9,22 @@ class ArtikelController extends Controller
     public function index()
     {
     	// mengambil data dari table pegawai
-    	$pegawai = DB::table('data_spbe')->get();
+    	$artikel = DB::table('data_spbe')->get();
  
     	// mengirim data pegawai ke view index
-    	return view('index',['pegawai' => $pegawai]);
+    	return view('index',['pegawai' => $artikel]);
  
     }
 
     public function store(Request $request)
 {
 	// insert data ke table pegawai
-	DB::table('pegawai')->insert([
-		'pegawai_nama' => $request->nama,
-		'pegawai_jabatan' => $request->jabatan,
-		'pegawai_umur' => $request->umur,
-		'pegawai_alamat' => $request->alamat
+	DB::table('data_spbe')->insert([
+		'id' => $request->id,
+		'kd_artikel' => $request->kd_artikel,
+		'judul' => $request->judul,
+		'tgl_upload' => $request->tgl_upload,
+        'gambar' => $request->gambar
 	]);
 	// alihkan halaman ke halaman pegawai
 	return redirect('/pegawai');
